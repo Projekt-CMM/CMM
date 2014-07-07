4feefa7f1ba2bdbb1b3049c8bfc8961449a297e4
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2014-07-07 16:51:15 +0200

    extending test-file

--- test/test01.c
+++ test/test01.c
@@ -5,6 +5,18 @@
 * @date 7 July 2014
 */
 
+struct Point {
+	int x, y;
+}
+
+Point p;
+char ch;
+float f;
+
+const int i = 1;
+const float fc = 1.2;
+const char c = 'd';
+
 void main() {
 	int a,b,c;
 	a = 1;
