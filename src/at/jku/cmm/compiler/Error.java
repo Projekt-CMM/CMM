1e36b1a62b949f2c591b2d99d0264052bd611657
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2014-07-15 10:23:02 +0200

    make at.jku.cmm.compiler.error public

--- src/at/jku/cmm/compiler/Error.java
+++ src/at/jku/cmm/compiler/Error.java
@@ -7,11 +7,11 @@ Holds information about a syntax error or a semantic error in a C-- program.
 --------------------------------------------------------------------------------*/
 
 public class Error {
-	int    line;
-	int    col;
-	String msg;
-	Error  next;
-	Error(int line, int col, String msg) {
+	public int    line;
+	public int    col;
+	public String msg;
+	public Error  next;
+	public Error(int line, int col, String msg) {
 		this.line = line; this.col = col; this.msg = msg;
 	}
 }
