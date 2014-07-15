8b312742d1519839f1b3f28e8b8674b8f33f9a7f
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2014-07-15 15:06:17 +0200

    fix coco.sh and CMM.atg to new package path

--- src/coco.sh
+++ src/coco.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cocoj CMM.atg
-mv Parser.java at/jku/cmm/compiler/Parser.java
-mv Scanner.java at/jku/cmm/compiler/Scanner.java
+mv Parser.java at/jku/ssw/cmm/compiler/Parser.java
+mv Scanner.java at/jku/ssw/cmm/compiler/Scanner.java
