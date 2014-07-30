eb654931c299bfb7440d7d746d37f7d7bd7b2407
Peter Wassermair <Peda1996@gmail.com>
2014-07-30 09:59:11 +0200

    handler Features added

--- src/at/jku/ssw/cmm/filemanagenment/ProfileContentHandler.java
+++ src/at/jku/ssw/cmm/filemanagenment/ProfileContentHandler.java
@@ -14,6 +14,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 import com.sun.org.apache.xerces.internal.parsers.DOMParser;
 
@@ -27,12 +28,14 @@ import javax.xml.transform.Transformer;
 import org.w3c.dom.Element;
 
 import at.jku.ssw.cmm.profile.Profile;
+import at.jku.ssw.cmm.quests.datastructs.Settings;
 
 public class ProfileContentHandler {
 	
 	private Profile profilevar;
 	private String sep = System.getProperty("file.separator");
 	private ArrayList<String> finished = new ArrayList<>();
+	private	ArrayList<String> selectable = new ArrayList<>();
 	private NodeHandler handler = new NodeHandler();
 
 	/**
@@ -42,19 +45,23 @@ public class ProfileContentHandler {
 	 * 
 	 * @return
 	 * Parsed Profile, Datatype <b>Profile</b>
+	 * @throws IOException 
+	 * @throws SAXException 
 	 */
 	
-	public Profile Parse(String initPath, String profilePath, String file) throws ParserConfigurationException, SAXException, IOException{	
+	public Profile Parse(String initPath, String profilePath, String file) throws SAXException, IOException {	
+		try{
 		profilevar = new Profile();
 		profilevar.setPath(file);	
-		
+
 		 DOMParser parser = new DOMParser();
 		 parser.parse(initPath + sep + profilePath + sep + file);
-		 Document doc = parser.getDocument();
 		 
-		 NodeList root = doc.getChildNodes();		 
+		 Document doc = parser.getDocument();
+		 NodeList root = doc.getChildNodes();		
+
 		 List<Node> profile = handler.getNodeList(Profile.XML_PROFILE,root);
-		  
+ 
 		 for(int x = 0; x < profile.size();x++){	
 		 			Node currentProfile = profile.get(x);
 		 			NodeList nodes = currentProfile.getChildNodes();
@@ -62,35 +69,55 @@ public class ProfileContentHandler {
 		 			try{
 		 				profilevar.setNick(handler.getNodeValue(Profile.XML_NICK,nodes).get(0));
 		 			}catch(IndexOutOfBoundsException e){
-		 				System.out.println("No Name set - setting name to not set");
+		 				if(Settings.debug)System.out.println("No Name set - setting name to not set");
 		 				profilevar.setNick("not set");
 		 			}
 		 			
 		 			try{
 		 				profilevar.setXp(Integer.parseInt(handler.getNodeValue(Profile.XML_XP,nodes).get(0)));
 		 			}catch(IndexOutOfBoundsException e){
-		 				System.out.println("Curently no XP set - setting XP to 0");
+		 				if(Settings.debug)System.out.println("Curently no XP set - setting XP to 0");
 		 				profilevar.setXp(0);
 		 			}
 		 			try{
-		 				for(int i = 0; i< handler.getNodeValue(Profile.XML_FINISHED,nodes).size();i++)
-		 				finished.add(handler.getNodeValue("finished",nodes).get(i));
+		 				profilevar.setOpenedQuest(handler.getNodeValue(Profile.XML_OPENEDQUEST,nodes).get(0));
+		 			}catch(IndexOutOfBoundsException e){
+		 				if(Settings.debug)System.out.println("No Name set - setting name to not set");
+		 				profilevar.setOpenedQuest("no opened quest");
+		 			}
+		 			try{
+						for(int i = 0; i< handler.getNodeValue(Profile.XML_SELECTABLE,nodes).size();i++)
+		 				selectable.add(handler.getNodeValue(Profile.XML_SELECTABLE,nodes).get(i));
 					}catch(IndexOutOfBoundsException e){
-		 				System.out.println("No finished Quests");
+						if(Settings.debug)System.out.println("No selectable Quests");
+		 			}
+		 			try{
+		 				for(int i = 0; i< handler.getNodeValue(Profile.XML_FINISHED,nodes).size();i++)
+		 				finished.add(handler.getNodeValue(Profile.XML_FINISHED,nodes).get(i));
+					}catch(IndexOutOfBoundsException e){
+						if(Settings.debug)System.out.println("No finished Quests");
 		 			}
 		 			
 		 			try{
 		 				profilevar.setSelectedImage(handler.getNodeValue(Profile.XML_SELECTEDIMAGE,nodes).get(0));
 					}catch(IndexOutOfBoundsException e){
-		 				System.out.println("No Current Profile Image - setting Profile Image to none");
+						if(Settings.debug)System.out.println("No Current Profile Image - setting Profile Image to none");
 		 				profilevar.setSelectedImage("none");
 		 			}
 		 			
 		 }
-
+		
+		profilevar.setSelectable(selectable);
 		profilevar.setFinishedQuests(finished);
 		return profilevar;
-	}
+		
+		//Creates a new Profile if the file is not excising
+		}catch(SAXParseException e){
+			Write(profilevar, initPath + sep + profilePath + sep + file);
+			Parse(initPath,  profilePath,  file);
+			return profilevar;
+		}	
+}
 	
 	/**
 	 * 
@@ -125,13 +152,31 @@ public class ProfileContentHandler {
 					selectedimage.appendChild(doc.createTextNode("not set"));
 				profilevar.appendChild(selectedimage);
 		 
-				// firstname elements
-				Element firstname = doc.createElement("xp");
-				firstname.appendChild(doc.createTextNode(profile.getXp()+""));
-				profilevar.appendChild(firstname);
-		 
+				// XP elements
+				Element xp = doc.createElement(Profile.XML_XP);
+				xp.appendChild(doc.createTextNode(profile.getXp()+""));
+				profilevar.appendChild(xp);
+				
+				// Opened-Quest
+				Element openedQuest = doc.createElement(Profile.XML_OPENEDQUEST);
+				if(profile.getOpenedQuest() != null)
+					openedQuest.appendChild(doc.createTextNode(profile.getOpenedQuest()));
+				else
+					openedQuest.appendChild(doc.createTextNode("no opened Quest"));
+				
+				profilevar.appendChild(openedQuest);				
+				
+				// Selectable quest elements				
+				if(profile.getSelectableQuestNames() != null)
+				for(int i = 0; i < profile.getSelectableQuestNames().size();i++){
+					Element selectable = doc.createElement(Profile.XML_SELECTABLE);
+					selectable.appendChild(doc.createTextNode(profile.getSelectableQuestNames().get(i)));
+					profilevar.appendChild(selectable);
+				}	
+				
+				// Finished elements	
+				if(profile.getSelectableQuestNames() != null)
 				for(int i = 0; i < profile.getFinishedQuestNames().size();i++){
-					// Finished elements
 					Element finished = doc.createElement(Profile.XML_FINISHED);
 					finished.appendChild(doc.createTextNode(profile.getFinishedQuestNames().get(i)));
 					profilevar.appendChild(finished);
@@ -148,7 +193,7 @@ public class ProfileContentHandler {
 				//For saving the Result
 				transformer.transform(source, result);
 		 
-				System.out.println("File saved! in: " + filePath);
+				if(Settings.debug)System.out.println("File saved! in: " + filePath);
 		 
 			  } catch (ParserConfigurationException  | TransformerException pce) {
 				pce.printStackTrace();
