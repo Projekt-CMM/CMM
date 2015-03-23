1d6d7053555afaa247e56dc83a780642246807b5
Fabian Hummer <f.hummer@traunseenet.at>
2015-03-23 14:55:54 +0100

    Fixed UTF-8 Translation problem on Windows

--- src/at/jku/ssw/cmm/gettext/Language.java
+++ src/at/jku/ssw/cmm/gettext/Language.java
@@ -22,8 +22,10 @@
 package at.jku.ssw.cmm.gettext;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -81,7 +83,8 @@ public class Language {
 		
 		// Start reading file
 		try {
-			file = new BufferedReader(new FileReader("po/" + langCode ));
+			//file = new BufferedReader(new FileReader( ));
+			file = new BufferedReader(new InputStreamReader(new FileInputStream(new File("po/" + langCode)), "UTF8"));
 			while ((line = file.readLine()) != null) {
 				
 				//Load a translated String
