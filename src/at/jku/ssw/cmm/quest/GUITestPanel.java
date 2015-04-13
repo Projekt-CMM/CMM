51a7ae96deaad19fecb54ea70901d19be4f10427
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2015-04-13 15:26:48 +0200

    [fix] missing import

--- src/at/jku/ssw/cmm/quest/GUITestPanel.java
+++ src/at/jku/ssw/cmm/quest/GUITestPanel.java
@@ -15,6 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
