e09f5d206c8cd830b04825ab1fd0f15e9cec1d9f
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2015-04-21 10:17:13 +0200

    [enh] catch unknow compiler exceptions

--- src/at/jku/ssw/cmm/gui/debug/GUIdebugPanel.java
+++ src/at/jku/ssw/cmm/gui/debug/GUIdebugPanel.java
@@ -270,7 +270,13 @@ public class GUIdebugPanel {
 		);
 
 		// Compile and run
-		Tab table = CompileManager.compile(sourceCode, this.main, true);
+		Tab table = null;
+		try{
+			table = CompileManager.compile(sourceCode, this.main, true);
+		}catch(Exception e){
+			DebugShell.out(State.ERROR, Area.COMPILER, "Unknown compiler error. Compiler crashed.");
+			return false;
+		}
 		if (table != null)
 			return this.runManager.runInterpreter(ctrlPanel.getListener(),
 					new IOstream(this.main), table);
